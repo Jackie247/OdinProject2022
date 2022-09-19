56a976691b1c73f45ae2034d741bff18474d0106
Jackie Yu <jackieyu247@gmail.com>
2022-09-19 20:38:20 +0100

    update object creation structure js file

--- javascript/library/js/app.js
+++ javascript/library/js/app.js
@@ -1,28 +1,35 @@
-let myLibrary = [];
+// Library Class
+function Library(){
+    this.books = []
+}
+Library.prototype.addBookToLibrary = function (book){
+    // do stuff
+    this.books.push(book)
+}
 
+Library.prototype.show = function() {
+    this.books.forEach(function(entry){
+        console.log(entry);
+    });
+}
+
+// Book Class
 function Book (title,author){
     // constructor
     this.title = title;
     this.author = author;
 }
+// Library object creation
+var library = new Library();
+// Book oject creation
+var book1 = new Book("Booky","J.K");
+var book2 = new Book("Chooky","T.K");
+var book3 = new Book("Rooky","B.K");
+// DOM Variables
+//let bookGrid = document.querySelector(".bookGrid");
 
-function addBookToLibrary(book){
-    // do stuff
-    myLibrary.push(book);
-}
+library.addBookToLibrary(book1)
+library.addBookToLibrary(book2)
+library.addBookToLibrary(book3)
 
-function displayLibrary(){
-    myLibrary.forEach(function(entry){
-        console.log(entry);
-    });
-}
-
-var Book1 = new Book("Booky","J.K");
-var Book2 = new Book("Chooky","T.K");
-var Book3 = new Book("Rooky","B.K");
-
-addBookToLibrary(Book1);
-addBookToLibrary(Book2);
-addBookToLibrary(Book3);
-
-displayLibrary();
+library.show();
