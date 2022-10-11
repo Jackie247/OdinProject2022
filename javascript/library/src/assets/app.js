746b1d02e79731d9c35e5b943f6431ac792d8121
Jackie Yu <jackieyu247@gmail.com>
2022-10-11 19:49:08 +0100

    update layout to use Class instead of object constructor

--- javascript/library/src/assets/app.js
+++ javascript/library/src/assets/app.js
@@ -9,88 +9,89 @@ let author = document.getElementById('author');
 let pages = document.getElementById('pages');
 let read = document.getElementById('readCheckbox');
 // Library Class
-function Library(){
-    // array of book objects
-    this.booksArray = [];
-}
-Library.prototype.addBookToLibrary = function (book){
-    if(this.isInLibrary(book) == false){
-        this.booksArray.push(book);
-        this.addBookToGrid(book);
-    }
-}
-Library.prototype.removeBookFromLibrary = function(book){
-    this.booksArray.splice(this.booksArray.findIndex(bookInArray => bookInArray.title === book.title),1);
-}
-Library.prototype.addBookToGrid = function(book) {
-    console.log(book);
-    console.log(book.title);
-    // Create the card elements
-    let card = document.createElement("div");
-    let title = document.createElement("h3");
-    let author = document.createElement("p");
-    let pages = document.createElement("p");
-    let btnContainer = document.createElement("div");
-    let read = document.createElement("button");
-    let remove = document.createElement("button");
-    if(book.read){
-        read.textContent = 'Finished';
-        read.classList.add("read");
-    }
-    else{
-        read.textContent = 'Unfinished'
-        read.classList.add("not-read")
-    }
-    read.addEventListener('click',()=>{
-        if(read.classList.contains("read")){
-            read.classList.remove("read");
-            read.classList.add("not-read");
-            read.textContent = 'Unfinished'
+class Library{
+    booksArray = []
+    constructor(){}
+    addBookToLibrary = (book) => {
+        if(this.isInLibrary(book) == false){
+            this.booksArray.push(book);
+            this.addBookToGrid(book);
         }
-        else if(read.classList.contains("not-read")){
-            read.classList.remove("not-read");
-            read.classList.add("read");
+    }
+    removeBookFromLibrary = (book) => {
+        this.booksArray.splice(this.booksArray.findIndex(bookInArray => bookInArray.title === book.title),1);
+    }
+    addBookToGrid = (book) => {
+        console.log(book);
+        console.log(book.title);
+        // Create the card elements
+        let card = document.createElement("div");
+        let title = document.createElement("h3");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        let btnContainer = document.createElement("div");
+        let read = document.createElement("button");
+        let remove = document.createElement("button");
+        if(book.read){
             read.textContent = 'Finished';
+            read.classList.add("read");
         }
-    })
-    remove.addEventListener('click',()=>{
-        this.removeBookFromLibrary(book);
-        bookGrid.removeChild(card);
-    })
-    // Add classes to elements
-    card.classList.add("bookCard");
-    btnContainer.classList.add("card-btn-container")
-    remove.classList.add("remove");
-    // Update text content for elements
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = `${book.pages} pages`;
-    remove.textContent = 'Delete';
-    // Append buttons to btn div
-    btnContainer.appendChild(read);
-    btnContainer.appendChild(remove);
-    // Append book text to card div
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(btnContainer);
-    
-    bookGrid.appendChild(card);
-    form.reset();
-}
-Library.prototype.isInLibrary = function(book){
-    if(this.booksArray.some(bookInArray => bookInArray.title === book.title)){
-        return true;
+        else{
+            read.textContent = 'Unfinished'
+            read.classList.add("not-read")
+        }
+        read.addEventListener('click',()=>{
+            if(read.classList.contains("read")){
+                read.classList.remove("read");
+                read.classList.add("not-read");
+                read.textContent = 'Unfinished'
+            }
+            else if(read.classList.contains("not-read")){
+                read.classList.remove("not-read");
+                read.classList.add("read");
+                read.textContent = 'Finished';
+            }
+        })
+        remove.addEventListener('click',()=>{
+            this.removeBookFromLibrary(book);
+            bookGrid.removeChild(card);
+        })
+        // Add classes to elements
+        card.classList.add("bookCard");
+        btnContainer.classList.add("card-btn-container")
+        remove.classList.add("remove");
+        // Update text content for elements
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = `${book.pages} pages`;
+        remove.textContent = 'Delete';
+        // Append buttons to btn div
+        btnContainer.appendChild(read);
+        btnContainer.appendChild(remove);
+        // Append book text to card div
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(btnContainer);
+        
+        bookGrid.appendChild(card);
+        form.reset();
     }
-    return false;
+    isInLibrary = (book) => {
+        if(this.booksArray.some(bookInArray => bookInArray.title === book.title)){
+            return true;
+        }
+        return false;
+    }
 }
 // Book Class
-function Book (title,author,pages,read){
-    // constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(title,author,pages,read){
+        this.title=title;
+        this.author=author;
+        this.pages=pages;
+        this.read=read;
+    }   
 }
 // Library object creation
 var library = new Library();
