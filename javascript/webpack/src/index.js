a01f94a312f9889d3fae8200f160e4dc3b478918
Jackie Yu <jackieyu247@gmail.com>
2022-11-23 09:27:05 +0000

    add

--- javascript/webpack/src/index.js
+++ javascript/webpack/src/index.js
@@ -1,9 +1,11 @@
 import myName from './myName';
 import './style.css';
 import Icon from './camping.png';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.textContent = myName('Cody');
     element.classList.add('hello');
@@ -11,7 +13,10 @@ function component() {
     // Add the image to our existing div.
     const myIcon = new Image();
     myIcon.src = Icon;
-
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
     element.appendChild(myIcon);
 
     return element;
